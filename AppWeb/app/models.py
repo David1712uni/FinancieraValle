d01ec4ee5e35fa19c477c65f247ddb820e407946
David Caruzo Cieza <david1237891712@gmail.com>
2024-10-06 17:13:04 -0500

    Update models.py

--- AppWeb/app/models.py
+++ AppWeb/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class AsientoContable(models.Model):
     fecha = models.DateField()
     cuenta = models.CharField(max_length=255)
-    tipo_cuenta = models.CharField(max_length=10, choices=[('AC', 'Activo Circulante'), ('ANC', 'Activo No Circulante'), ('P', 'Pasivo'), ('PT', 'Patrimonio'), ('G', 'Gastos'), ('C', 'Costo'), ('I', 'Ingresos')])
+    tipo_cuenta = models.CharField(max_length=10, choices=[('AC', 'Activo Circulante'), ('ANC', 'Activo No Circulante'), ('P', 'Pasivo'), ('PT', 'Patrimonio'), ('G', 'Gastos'), ('C', 'Costo'), ('I', 'Ingresos'), ('CC', 'Cuentas de Cierre'), ('CEA', 'Cuentas Analíticas de Explotación')])
     tipo_monto = models.CharField(max_length=5, choices=[('Debe', 'Debe'), ('Haber', 'Haber')])
     monto = models.FloatField()
     
