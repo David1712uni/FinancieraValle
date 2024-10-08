b78b342b2401d721c8182bcd450253c42ccbd167
David Caruzo Cieza <david1237891712@gmail.com>
2024-10-07 20:07:19 -0500

    Update urls.py

--- AppWeb/app/urls.py
+++ AppWeb/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('resultados/', views.mostrar_resultados, name='resultados'),
+    path('saldo_inicial/', views.elegir_saldos, name='saldo_inicial'),
 ]
