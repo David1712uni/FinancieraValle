d9699d9c0a08cad4d58102a6fd9e7607da885fcd
David Caruzo Cieza <david1237891712@gmail.com>
2024-10-08 18:17:29 -0500

    Update views.py

--- AppWeb/app/views.py
+++ AppWeb/app/views.py
@@ -463,10 +463,9 @@ def mostrar_resultados(request):
             continue
 
         if  40 <= int(tipo_cuenta) and int(tipo_cuenta) <= 59 :
-            monto = asiento.monto if -asiento.tipo_monto == 'Debe' else asiento.monto
+            monto = -asiento.monto if asiento.tipo_monto == 'Debe' else asiento.monto
         else:
             monto = asiento.monto if asiento.tipo_monto == 'Debe' else -asiento.monto
-        
 
         if cuenta== 'AC':
             if tipo_cuenta not in activos_corrientes:
